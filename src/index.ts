bf9e477e4af66a5b25dec3b328ab8da0e399b1c8
tomz <spirit108@foxmail.com>
2020-04-04 15:33:31 +0800

    枚举

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,10 @@
  * @Author: tom-z(spirit108@foxmail.com)
  * @Date: 2020-03-09 09:35:31
  * @LastEditors: tom-z(spirit108@foxmail.com)
- * @LastEditTime: 2020-04-02 23:22:00
+ * @LastEditTime: 2020-04-04 14:51:51
  */
-require("./js/var");
-require("./js/interface");
-require("./js/t");
-require("./js/class");
+require("./js/var")
+require("./js/interface")
+require("./js/t")
+require("./js/class")
+require("./js/enum")
